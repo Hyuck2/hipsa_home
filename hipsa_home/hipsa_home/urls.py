81175c5bc278fa76e8621e7241c1fee95855ce83
hyuck2 <hyuck2@outlook.kr>
2020-06-02 16:33:47 +0900

    add blog

--- hipsa_home/hipsa_home/urls.py
+++ hipsa_home/hipsa_home/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from group_meeting import views as group_meeting_views
 from references import views as references_views
+from blog import views as blog_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -15,4 +16,5 @@ urlpatterns = [
     path('references/', references_views.home, name = 'home'),
 
     # blog urls
+    path('blog/', blog_views.home, name = 'home'),
 ]
