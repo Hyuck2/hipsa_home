3a3a772badc58bd879c1119887ee0620e96c9fcd
hyuck2 <hyuck2@outlook.kr>
2020-06-10 00:48:55 +0900

    model

--- hipsa_home/group_meeting/models.py
+++ hipsa_home/group_meeting/models.py
@@ -3,9 +3,6 @@ from django.db import models
 from django.utils import timezone
 
 class Meeting(models.Model):
-    date = models.CharField(max_legth = 200)
-    attendent = models.CharField(max_length = 200)
-    
     def get_meetings(self):
         self.meetings = 0
 
